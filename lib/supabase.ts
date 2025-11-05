106f681c47030717bc9baccf61b535be9ab447e3
cheickkone26 <102957239+cheickkone26@users.noreply.github.com>
2025-11-05 11:59:49 +0100

    up supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = https://stpzshlulvqodmlikryh.supabase.co
-const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0cHpzaGx1bHZxb2RtbGlrcnloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyNzc2OTEsImV4cCI6MjA3Nzg1MzY5MX0.40gC6eDKCiNGtyk-JflYTJxtvXq5Tv3FjaD2PFD5cp4
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
 
